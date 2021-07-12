a8569aa4c353bb09143c0bf2d701a4ac6cd40acd
Ainesh Sootha <aineshsootha@gmail.com>
2021-07-12 00:11:03 -0400

    Made updates to swiftui

---
+++ swiftStuff/frenchinator/TranslatedView.swift
@@ -0,0 +1,20 @@
+//
+//  TranslatedView.swift
+//  frenchinator
+//
+//  Created by Ainesh Sootha on 7/11/21.
+//
+
+import SwiftUI
+
+struct TranslatedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TranslatedView_Previews: PreviewProvider {
+    static var previews: some View {
+        TranslatedView()
+    }
+}
