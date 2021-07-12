2e7ecbd873b001407471e5af5460f3c8113030c5
Ainesh Sootha <aineshsootha@gmail.com>
2021-07-12 15:40:35 -0400

    added figma

--- swiftStuff/frenchinator/ContentView.swift
+++ swiftStuff/frenchinator/ContentView.swift
@@ -7,84 +7,77 @@
 
 import SwiftUI
 
-struct Untranslated: Encodable{
+class Untranslated: Encodable, ObservableObject{
     var word: String
+    init(worden:String){
+        word = worden
+    }
 }
 
-struct Translated: Decodable{
-    var en: String
-    var wordRefURL: String
-}
+
+
 
 struct ContentView: View {
+    @State var untranslated: Untranslated = Untranslated(worden:"")
     @State private var word: String = ""
     @State private var en_word = ""
     @State private var wordRefURL = ""
+    @State private var showingTranslated = false
+    @State private var headW = CGFloat(400.0)
+    @State private var headH = CGFloat(250.0)
     
+    let mainW = CGFloat(283.0)
+    init() {
+            UITextView.appearance().backgroundColor = .clear
+        }
     var body: some View {
         ZStack{
-            Color(red: 3.0 / 255, green: 29.0 / 255, blue: 68.0 / 255)
+            Color(red: 127.0 / 255, green: 198.0 / 255, blue: 164.0 / 255)
             .edgesIgnoringSafeArea(.all)
             VStack(alignment: .center){
                 HStack{
                     Text("Frenchinator")
                         .font(Font.custom("Roboto-Black", size:40))
-                        .foregroundColor(Color(red: 216.0 / 255, green: 17.0 / 255, blue: 89.0 / 255))
-                }
-                Spacer()
+                        .foregroundColor(Color(red: 9.0 / 255, green: 56.0 / 255, blue: 36.0 / 255))
+                        .position(x:headW / 2, y:headH / 2)
+                        
+                }.frame(width: headW, height: headH, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 HStack{
-                    Text("Bounjour!")
-                        .font(Font.custom("Roboto-Black", size:30))
-                        .foregroundColor(Color(red: 216.0 / 255, green: 17.0 / 255, blue: 89.0 / 255))
-                }
-                .padding()
-                HStack{
-                    TextField("Le mot",
-                              text: $word
-                    ).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextEditor(text: $word)
+                        .background(Color(red: 127.0 / 255, green: 198.0 / 255, blue: 164.0 / 255))
+                    .foregroundColor(Color(red: 9.0 / 255, green: 56.0 / 255, blue: 36.0 / 255))
+                    .font(Font.custom("Roboto-Black", size:20))
                     .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color(red: 9.0 / 255, green: 56.0 / 255, blue: 36.0 / 255), lineWidth: 4.0))
+                    .frame(width: mainW, height: 159, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
                 }
                 HStack{
-                    Button(
-                        action: {self.translate()},
-                        label: { Text("Click Me") }
-                    )
-                }
+                    Button(action: {
+                        untranslated = Untranslated(worden: self.word)
+                        self.showingTranslated = true
+                    }){
+                        VStack{
+                        Text("Translate")
+                            .font(Font.custom("Roboto-Black", size:20))
+                            .foregroundColor(Color(red: 252.0 / 255, green: 208.0 / 255, blue: 161.0 / 255))
+                        
+                        }
+                        .frame(width: mainW, height: 57)
+                        .background(Color(red: 9.0 / 255, green: 56.0 / 255, blue: 36.0 / 255))
+                        .cornerRadius(60)
+                    }.position(x: headW / 2, y: headH / 2)
+                }.frame(width: headW, height: headH, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 Spacer()
-                HStack{
-                    Text(self.en_word)
-                    Text(self.wordRefURL)
-                }
+                
                 Spacer()
             }
-        }
+        }.sheet(isPresented: $showingTranslated, content: {
+            //Show translated
+            TranslatedView(untranslated: self.untranslated)
+        })
     }
-    func translate(){
-        let en_word = Untranslated(word: word)
-        guard let encoded = try? JSONEncoder().encode(en_word)
-            else{
-                print("Failed to encode")
-                return
-                }
-        let url = URL(string: "http://192.168.4.21:5000/translate")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-        request.httpBody = encoded
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
-                return
-            }
-            if let decodedEn = try? JSONDecoder().decode(Translated.self, from: data) {
-                self.en_word = decodedEn.en
-                self.wordRefURL = decodedEn.wordRefURL
-            } else {
-                print("Invalid response from server \(data)")
-            }
-        }.resume()
-    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
--- swiftStuff/frenchinator/TranslatedView.swift
+++ swiftStuff/frenchinator/TranslatedView.swift
@@ -7,14 +7,78 @@
 
 import SwiftUI
 
+class Translated: Decodable, ObservableObject{
+    var en: String = "Not found"
+    var wordRefURL: String = "Not found"
+}
+
 struct TranslatedView: View {
+    @State var untranslated: Untranslated
+    @State private var translated: Translated = Translated()
+    @State private var word: String = ""
+    @State private var en_word = ""
+    @State private var wordRefURL = ""
+    @State private var headW = CGFloat(400.0)
+    @State private var headH = CGFloat(250.0)
+    let mainW = CGFloat(283.0)
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color(red: 9.0 / 255, green: 56.0 / 255, blue: 36.0 / 255)
+            .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .center){
+                HStack{
+                    Text("Frenchinator")
+                        .font(Font.custom("Roboto-Black", size:40))
+                        .foregroundColor(Color(red: 252.0 / 255, green: 208.0 / 255, blue: 161.0 / 255))
+                        .position(x:headW / 2, y:headH / 2)
+                        
+                }.frame(width: headW, height: headH, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                HStack{
+                    Text(word)
+                        .frame(width: mainW, height: 159, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .lineLimit(nil)
+                        .multilineTextAlignment(.leading)
+                        .fixedSize()
+                        .background(Color(red: 9.0 / 255, green: 56.0 / 255, blue: 36.0 / 255))
+                        
+                    .foregroundColor(Color(red: 252.0 / 255, green: 208.0 / 255, blue: 161.0 / 255))
+                    .font(Font.custom("Roboto-Black", size:20))
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color(red: 252.0 / 255, green: 208.0 / 255, blue: 161.0 / 255), lineWidth: 4.0))
+                    
+                    
+                }.onAppear{self.translate()}
+                Spacer()
+            }
+        }
+    }
+    func translate(){
+        let en_word = untranslated
+        guard let encoded = try? JSONEncoder().encode(en_word)
+            else{
+                print("Failed to encode")
+                return
+                }
+        let url = URL(string: "http://192.168.4.21:5000/translate")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            if let decodedEn = try? JSONDecoder().decode(Translated.self, from: data) {
+                self.word = decodedEn.en
+                self.translated.wordRefURL = decodedEn.wordRefURL
+            } else {
+                print("Invalid response from server \(data)")
+            }
+        }.resume()
     }
 }
 
-struct TranslatedView_Previews: PreviewProvider {
-    static var previews: some View {
-        TranslatedView()
-    }
-}
+
